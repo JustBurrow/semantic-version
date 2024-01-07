991dc77cb7a20de040c7127f84c2b1cecdb93d06
Just Burrow <just.burrow@lul.kr>
2024-01-07 14:24:36 +0900

    🔥 샘플 파일 삭제.

--- shared/src/commonMain/kotlin/kr/lul/version/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package kr.lul.version
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/kr/lul/version/Platform.kt
+++
@@ -1,7 +0,0 @@
-package kr.lul.version
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
