d624d158aab80b001945ddee06140578b2e9b7ec
Just Burrow <just.burrow@lul.kr>
2024-01-20 10:28:54 +0900

    👷 `artifactId` 이름 충돌 해소.
    ```
    > Task :shared:publishKotlinMultiplatformPublicationToMavenLocal
    Multiple publications with coordinates 'kr.lul:semantic-version:0.0.1' are published to repository 'mavenLocal'. The publications 'jvm' in project ':shared' and 'kotlinMultiplatform' in project ':shared' will overwrite each other!
    ```

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -60,7 +60,7 @@ kotlin {
         publications {
             withType<MavenPublication> {
                 groupId = "kr.lul"
-                artifactId = rootProject.name
+                artifactId = "${rootProject.name}-$name"
                 version = "0.0.1"
 
                 pom {
